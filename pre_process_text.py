44640496b7db79fd4a628738dafb0b4370ff26cf
tracy rohlin <tracy.rohlin@gmail.com>
2017-04-09 23:00:52 -0700

    added file to pickle text

---
+++ pre_process_text.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import pickle
+from collections import Counter
+
+def main():
+    comment_type = ["aggression", "toxicity", "attack"]
+    for c_type in comment_type:
+        comments = pd.read_csv(c_type + '_annotated_comments.tsv', sep = '\t', index_col = 0)
+        annotations = pd.read_csv(c_type + '_annotations.tsv',  sep = '\t')
+        labels = annotations.groupby('rev_id')[c_type].mean() > 0.5
+        comments[c_type] = labels
+        comments['comment'] = comments['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
+        comments['comment'] = comments['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
+        comments['comment'] = comments['comment'].apply(lambda x: x.replace("`", " "))
+
+        comments.to_pickle(c_type + ".pkl")
+
+if __name__ == "__main__":
+    main()
