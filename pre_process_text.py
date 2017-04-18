e8b5d28ef39d58b8329f0ec66eb3b19fcefafd0e
cmansfield8 <cmansfield8@gmail.com>
2017-04-18 11:03:51 -0700

    removed loop from pre_process_text

--- pre_process_text.py
+++ pre_process_text.py
@@ -4,19 +4,18 @@ import pandas as pd
 import pickle
 from collections import Counter
 
-def main():
-    # Only doing one type... remove loop entirely?
-    comment_type = ["attack"]
-    for c_type in comment_type:
-        comments = pd.read_csv(c_type + '_annotated_comments.tsv', sep = '\t', index_col = 0)
-        annotations = pd.read_csv(c_type + '_annotations.tsv',  sep = '\t')
-        labels = annotations.groupby('rev_id')[c_type].mean() > 0.5
-        comments[c_type] = labels
-        comments['comment'] = comments['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
-        comments['comment'] = comments['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
-        comments['comment'] = comments['comment'].apply(lambda x: x.replace("`", " "))
 
-        comments.to_pickle(c_type + ".pkl")
+def main():
+    comment_type = ["attack"]
+    comments = pd.read_csv('attack_annotated_comments.tsv', sep = '\t', index_col = 0)
+    annotations = pd.read_csv('attack_annotations.tsv',  sep = '\t')
+    labels = annotations.groupby('rev_id')[comment_type].mean() > 0.5
+    comments[comment_type] = labels
+    comments['comment'] = comments['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
+    comments['comment'] = comments['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
+    comments['comment'] = comments['comment'].apply(lambda x: x.replace("`", " "))
+
+    comments.to_pickle("attack.pkl")
 
 if __name__ == "__main__":
     main()
